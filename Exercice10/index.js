22bfdab098ca593362ee2500f803b757e244b3d4
ACH90 <chassagne.alexandre@gmail.com>
2024-08-29 15:51:51 +0200

    exo 10 ok

--- Exercice10/index.js
+++ Exercice10/index.js
@@ -1,3 +1,14 @@
 //votre code ici
 
-export default printNumbers
+function printNumbers(n) {
+  let result = ""; //Initialiser chaine de caractères
+  for (let i = 1; i <= n; i++) {
+    //Boucle d'itération
+    result += i + " "; //Concatenation
+  }
+  return result; //Retourner la chaine de caractères finale
+}
+
+export default printNumbers;
+
+console.log(printNumbers(5));
